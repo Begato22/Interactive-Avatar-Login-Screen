758b5251e5771264dccad27f3d63275c9d7c29d4
Eslam Nour Eldeen <83145363+Begato22@users.noreply.github.com>
2024-09-01 20:45:35 +0300

    Separate widgets.

--- lib/login/login_screen.dart
+++ lib/login/login_screen.dart
@@ -50,14 +50,14 @@ class _LoginScreenState extends State<LoginScreen> {
                             ),
                           ),
                           const SizedBox(height: 30),
-                          defaultTextField(
+                          DefaultTextField(
                             controller: _cubit.emailController,
                             keyboardType: TextInputType.emailAddress,
                             prefix: Icons.email,
                             label: 'Email',
                           ),
                           const SizedBox(height: 10),
-                          defaultPasswordField(
+                          DefaultPasswordField(
                             controller: _cubit.passwordController,
                             keyboardType: TextInputType.visiblePassword,
                             prefix: Icons.lock,
@@ -67,7 +67,7 @@ class _LoginScreenState extends State<LoginScreen> {
                             obscureText: _cubit.isVisitable,
                           ),
                           const SizedBox(height: 10),
-                          defaultButton(onPressed: () async => await _cubit.reset(), label: 'login')
+                          LoginButton(onPressed: () async => await _cubit.reset(), label: 'login')
                         ],
                       ),
                     ),
@@ -77,102 +77,171 @@ class _LoginScreenState extends State<LoginScreen> {
       },
     );
   }
+}
 
-  Widget defaultButton({
-    required Function onPressed,
-    required String label,
-    bool isDisabled = false,
-    Color color = Colors.teal,
-  }) =>
-      SizedBox(
-        width: double.infinity,
-        height: 40.0,
-        child: ElevatedButton(
-          onPressed: () => isDisabled ? null : onPressed(),
-          style: ElevatedButton.styleFrom(backgroundColor: isDisabled ? Colors.grey : Colors.teal),
-          child: Text(label.toUpperCase()),
-        ),
-      );
+class LoginButton extends StatefulWidget {
+  final Function onPressed;
+  final String label;
+  final bool isDisabled = false;
+  final Color color = Colors.teal;
 
-  Widget defaultTextField({
-    required TextEditingController controller,
-    required TextInputType keyboardType,
-    required IconData prefix,
-    IconData? suffix,
-    Function? suffixFunc,
-    required String label,
-    bool obscureText = false,
-  }) =>
-      BlocConsumer<LoginCubit, LoginStates>(
-        listener: (context, state) {},
-        builder: (context, state) {
-          return TextFormField(
-            controller: controller,
-            keyboardType: TextInputType.emailAddress,
-            decoration: InputDecoration(
-              label: Text(label),
-              prefixIcon: Icon(prefix),
-              suffixIcon: GestureDetector(child: Icon(suffix), onTap: () => suffixFunc!()),
-              border: const OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(15.0))),
+  const LoginButton({
+    super.key,
+    required this.onPressed,
+    required this.label,
+    isDisabled = false,
+    color = Colors.teal,
+  });
+
+  @override
+  State<LoginButton> createState() => _LoginButtonState();
+}
+
+class _LoginButtonState extends State<LoginButton> {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      height: 40.0,
+      child: ElevatedButton(
+        onPressed: () => widget.isDisabled ? null : widget.onPressed(),
+        style: ElevatedButton.styleFrom(backgroundColor: widget.isDisabled ? Colors.grey : Colors.teal),
+        child: Text(widget.label.toUpperCase()),
+      ),
+    );
+  }
+}
+
+class DefaultTextField extends StatefulWidget {
+  final TextEditingController controller;
+  final TextInputType keyboardType;
+  final IconData prefix;
+  final IconData? suffix;
+  final Function? suffixFunc;
+  final String label;
+  final bool? obscureText;
+  const DefaultTextField({
+    super.key,
+    required this.controller,
+    required this.keyboardType,
+    required this.prefix,
+    required this.label,
+    this.suffix,
+    this.suffixFunc,
+    this.obscureText = false,
+  });
+
+  @override
+  State<DefaultTextField> createState() => _DefaultTextFieldState();
+}
+
+class _DefaultTextFieldState extends State<DefaultTextField> {
+  late LoginCubit _cubit;
+  @override
+  void initState() {
+    _cubit = context.read<LoginCubit>();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<LoginCubit, LoginStates>(
+      listener: (context, state) {},
+      builder: (context, state) {
+        return TextFormField(
+          controller: widget.controller,
+          keyboardType: TextInputType.emailAddress,
+          decoration: InputDecoration(
+            label: Text(widget.label),
+            prefixIcon: Icon(widget.prefix),
+            suffixIcon: GestureDetector(child: Icon(widget.suffix), onTap: () => widget.suffixFunc!()),
+            border: const OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(15.0))),
+          ),
+          obscureText: widget.obscureText ?? false,
+          onTap: () => _cubit.onEmailTapped(),
+          validator: (val) {
+            if (val!.isEmpty) {
+              return 'you must add ${widget.label}';
+            }
+            return null;
+          },
+          onChanged: (value) => _cubit.onEmailChanged(value),
+        );
+      },
+    );
+  }
+}
+
+class DefaultPasswordField extends StatefulWidget {
+  final TextEditingController controller;
+  final TextInputType keyboardType;
+  final IconData prefix;
+  final IconData? suffix;
+  final Function? suffixFunc;
+  final String label;
+  final bool obscureText;
+  const DefaultPasswordField({
+    super.key,
+    required this.controller,
+    required this.keyboardType,
+    required this.prefix,
+    this.suffix,
+    this.suffixFunc,
+    required this.label,
+    this.obscureText = false,
+  });
+
+  @override
+  State<DefaultPasswordField> createState() => _DefaultPasswordFieldState();
+}
+
+class _DefaultPasswordFieldState extends State<DefaultPasswordField> {
+  late LoginCubit _cubit;
+  @override
+  void initState() {
+    _cubit = context.read<LoginCubit>();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<LoginCubit, LoginStates>(
+      listener: (context, state) {},
+      builder: (context, state) {
+        return TextFormField(
+          controller: widget.controller,
+          keyboardType: TextInputType.emailAddress,
+          decoration: InputDecoration(
+            prefixIcon: Icon(widget.prefix),
+            suffixIcon: GestureDetector(
+              child: Icon(widget.suffix),
+              onTap: () => widget.suffixFunc!(),
             ),
-            obscureText: obscureText,
-            onTap: () => _cubit.onEmailTapped(),
-            validator: (val) {
-              if (val!.isEmpty) {
-                return 'you must add $label';
-              }
-              return null;
-            },
-            onChanged: (value) => _cubit.onEmailChanged(value),
-          );
-        },
-      );
-
-  Widget defaultPasswordField({
-    required TextEditingController controller,
-    required TextInputType keyboardType,
-    required IconData prefix,
-    IconData? suffix,
-    Function? suffixFunc,
-    required String label,
-    bool obscureText = false,
-  }) =>
-      BlocConsumer<LoginCubit, LoginStates>(
-        listener: (context, state) {},
-        builder: (context, state) {
-          return TextFormField(
-            controller: controller,
-            keyboardType: TextInputType.emailAddress,
-            decoration: InputDecoration(
-              prefixIcon: Icon(prefix),
-              suffixIcon: GestureDetector(
-                child: Icon(suffix),
-                onTap: () => suffixFunc!(),
-              ),
-              label: Text(label),
-              border: const OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(15.0))),
-            ),
-            obscureText: obscureText,
-            validator: (val) {
-              if (val!.isEmpty) {
-                return 'you must add $label';
-              }
-              return null;
-            },
-            onTap: () async => await _cubit.onPasswordTapped(),
-            onEditingComplete: () {
-              FocusManager.instance.primaryFocus?.unfocus();
-              _cubit.leavePasswordField();
-              for (int i = 0; i < 7; i++) {
-                Future.delayed(const Duration(milliseconds: 200)).then((value) {
-                  _cubit.decreaseIndex();
-                });
-              }
-              if (_cubit.emailController.text.isEmpty) {
-                _cubit.currentIndex = 0;
-              }
-            },
-          );
-        },
-      );
+            label: Text(widget.label),
+            border: const OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(15.0))),
+          ),
+          obscureText: widget.obscureText,
+          validator: (val) {
+            if (val!.isEmpty) {
+              return 'you must add ${widget.label}';
+            }
+            return null;
+          },
+          onTap: () async => await _cubit.onPasswordTapped(),
+          onEditingComplete: () {
+            FocusManager.instance.primaryFocus?.unfocus();
+            _cubit.leavePasswordField();
+            for (int i = 0; i < 7; i++) {
+              Future.delayed(const Duration(milliseconds: 200)).then((value) {
+                _cubit.decreaseIndex();
+              });
+            }
+            if (_cubit.emailController.text.isEmpty) {
+              _cubit.currentIndex = 0;
+            }
+          },
+        );
+      },
+    );
+  }
 }
