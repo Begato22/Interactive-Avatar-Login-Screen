b190cdb36a000fef5dce0eac9bd99dac3d369922
Begato22 <eslam.rali@gmail.com>
2025-05-05 16:45:53 +0300

    Render fixes ..

--- lib/login/cubit.dart
+++ lib/login/cubit.dart
@@ -1,6 +1,7 @@
+import 'dart:async';
+
 import 'package:animation_login/login/states.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class LoginCubit extends Cubit<LoginStates> {
@@ -12,13 +13,11 @@ class LoginCubit extends Cubit<LoginStates> {
 
   String basePath = 'assets/images/';
 
-  List<Image?> motionList = [];
-  int currentIndex = 0;
+  int currentIndex = 1;
   bool isVisitable = true;
 
   void init() async {
     _prepareControllers();
-    await _prepareMotrinList();
   }
 
   void _prepareControllers() {
@@ -26,56 +25,43 @@ class LoginCubit extends Cubit<LoginStates> {
     passwordController = TextEditingController();
   }
 
-  Future<void> _prepareMotrinList() async {
-    for (var i = 1; i <= 33; i++) {
-      motionList.add(await _loadImageFromAsset(_getImagePath(i)));
+  Future<void> prepareMotrinList(BuildContext context) async {
+    for (int i = 1; i <= 33; i++) {
+      String path = _getImagePath(i);
+      scheduleMicrotask(() async {
+        await precacheImage(AssetImage(path), context);
+      });
     }
 
     emit(Redraw());
   }
 
-  Future<Image?> _loadImageFromAsset(String assetPath) async {
-    try {
-      final ByteData data = await rootBundle.load(assetPath);
-      return Image.memory(
-        data.buffer.asUint8List(),
-        fit: BoxFit.contain, // Optional: Set how the image should be displayed
-      );
-    } catch (_) {
-      emit(Error());
-      return null;
-    }
-  }
-
-  String _getImagePath(int index) {
-    return '$basePath$index.jpg';
-  }
+  String _getImagePath(int index) => '$basePath$index.jpg';
 
   void changeVisibility() {
     isVisitable = !isVisitable;
     if (isVisitable) {
-      decreaseIndex();
+      decreaseIndex(decreaseValue: 3);
     } else {
-      increaseIndex();
+      increaseIndex(setValue: 33);
     }
     emit(Redraw());
   }
 
-  void increaseIndex() {
-    currentIndex++;
-
+  void increaseIndex({int? setValue, int? increaseValue}) {
+    currentIndex = setValue ?? currentIndex + (increaseValue ?? 1);
     emit(Redraw());
   }
 
-  void decreaseIndex() {
-    if (currentIndex != 0) {
-      currentIndex--;
-    }
+  void decreaseIndex({int? setValue, int? decreaseValue}) {
+    if (currentIndex == 1) return;
+
+    currentIndex = setValue ?? currentIndex - (decreaseValue ?? 1);
     emit(Redraw());
   }
 
   void enteredPasswordField() {
-    currentIndex = MotionClip.hideEyes.start;
+    currentIndex = currentIndex >= MotionClip.hideEyes.start ? currentIndex : MotionClip.hideEyes.start;
 
     emit(Redraw());
   }
@@ -88,23 +74,26 @@ class LoginCubit extends Cubit<LoginStates> {
   Future<void> onEmailTapped() async {
     if (emailController.text.isEmpty) {
       if (currentIndex != MotionClip.watchEmail.start) {
-        while (currentIndex != MotionClip.watchEmail.start) {
+        while (currentIndex != MotionClip.hideEyes.start) {
+          await Future.delayed(const Duration(milliseconds: 100)).then((value) {
+            decreaseIndex();
+          });
+        }
+        currentIndex = MotionClip.watchEmail.start;
+      }
+      return;
+    } else {
+      if (emailController.text.length >= MotionClip.watchEmail.end) {
+        currentIndex = MotionClip.watchEmail.end;
+      } else {
+        while (currentIndex != emailController.text.length) {
           await Future.delayed(const Duration(milliseconds: 70)).then((value) {
             decreaseIndex();
           });
         }
       }
-      return;
     }
-    if (emailController.text.length >= MotionClip.watchEmail.end) {
-      currentIndex = MotionClip.watchEmail.end;
-    } else {
-      while (currentIndex != emailController.text.length) {
-        await Future.delayed(const Duration(milliseconds: 70)).then((value) {
-          decreaseIndex();
-        });
-      }
-    }
+    emit(Redraw());
   }
 
   Future<void> onPasswordTapped() async {
@@ -118,7 +107,7 @@ class LoginCubit extends Cubit<LoginStates> {
 
   void onEmailChanged(String value) {
     if (value.length > currentIndex) {
-      if (currentIndex != MotionClip.hideEyes.start) increaseIndex();
+      if (currentIndex <= MotionClip.watchEmail.end) increaseIndex();
     } else {
       if (emailController.text.isEmpty) currentIndex = MotionClip.watchEmail.start;
       decreaseIndex();
@@ -138,20 +127,16 @@ class LoginCubit extends Cubit<LoginStates> {
   Future<void> palyMotion(bool query, MotionAction action) async {
     while (query) {
       await Future.delayed(const Duration(milliseconds: 100)).then((value) {
-        if (action.isIncrease) {
-          increaseIndex();
-        } else {
-          decreaseIndex();
-        }
+        action.isIncrease ? increaseIndex() : decreaseIndex();
       });
     }
   }
 }
 
 enum MotionClip {
-  watchEmail(0, 23),
-  hideEyes(24, 31),
-  showPassword(32, 33);
+  watchEmail(1, 25),
+  hideEyes(27, 31),
+  showPassword(31, 33);
 
   final int start;
   final int end;
--- lib/login/login_screen.dart
+++ lib/login/login_screen.dart
@@ -27,55 +27,59 @@ class _LoginScreenState extends State<LoginScreen> {
   }
 
   @override
+  void didChangeDependencies() {
+    _cubit.prepareMotrinList(context);
+    super.didChangeDependencies();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return BlocConsumer<LoginCubit, LoginStates>(
       listener: (context, state) {},
       builder: (context, state) {
         return Scaffold(
           body: Center(
-            child: _cubit.motionList.isEmpty
-                ? const CircularProgressIndicator()
-                : Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: SingleChildScrollView(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const SizedBox(height: 20),
-                          Center(
-                            child: CircleAvatar(
-                              backgroundColor: Colors.teal,
-                              radius: 72,
-                              child: CircleAvatar(
-                                radius: 70,
-                                backgroundImage: _cubit.motionList[_cubit.currentIndex]?.image,
-                              ),
-                            ),
-                          ),
-                          const SizedBox(height: 30),
-                          DefaultTextField(
-                            controller: _cubit.emailController,
-                            keyboardType: TextInputType.emailAddress,
-                            prefix: Icons.email,
-                            label: 'Email',
-                          ),
-                          const SizedBox(height: 10),
-                          DefaultPasswordField(
-                            controller: _cubit.passwordController,
-                            keyboardType: TextInputType.visiblePassword,
-                            prefix: Icons.lock,
-                            label: 'Password',
-                            suffix: _cubit.isVisitable ? Icons.visibility : Icons.visibility_off,
-                            suffixFunc: () => _cubit.changeVisibility(),
-                            obscureText: _cubit.isVisitable,
-                          ),
-                          const SizedBox(height: 10),
-                          LoginButton(onPressed: () async => await _cubit.reset(), label: 'login')
-                        ],
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 20),
+                    Center(
+                      child: CircleAvatar(
+                        backgroundColor: Colors.teal,
+                        radius: 72,
+                        child: CircleAvatar(
+                          radius: 70,
+                          backgroundImage: AssetImage("assets/images/${_cubit.currentIndex == 0 ? 1 : _cubit.currentIndex}.jpg"),
+                        ),
                       ),
                     ),
-                  ),
+                    const SizedBox(height: 30),
+                    DefaultTextField(
+                      controller: _cubit.emailController,
+                      keyboardType: TextInputType.emailAddress,
+                      prefix: Icons.email,
+                      label: 'Email',
+                    ),
+                    const SizedBox(height: 10),
+                    DefaultPasswordField(
+                      controller: _cubit.passwordController,
+                      keyboardType: TextInputType.visiblePassword,
+                      prefix: Icons.lock,
+                      label: 'Password',
+                      suffix: _cubit.isVisitable ? Icons.visibility : Icons.visibility_off,
+                      suffixFunc: () => _cubit.changeVisibility(),
+                      obscureText: _cubit.isVisitable,
+                    ),
+                    const SizedBox(height: 10),
+                    LoginButton(onPressed: () async => await _cubit.reset(), label: 'login')
+                  ],
+                ),
+              ),
+            ),
           ),
         );
       },
